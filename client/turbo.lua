b76ba638f98868e1691dcfa58579bb29cb759c63
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-17 10:47:45 -0800

    added skillbar config

--- client/turbo.lua
+++ client/turbo.lua
@@ -21,26 +21,88 @@ RegisterNetEvent('doj:client:applyTurbo', function()
 			playAnim("mini@repair", "fixing_a_ped", 35000)
 			SetVehicleEngineOn(vehicle, false, false,true)
 			SetVehicleDoorOpen(vehicle, 4, false, false)
-			local finished = exports["np-skillbar"]:taskBar(1700,math.random(3,7))
-			if finished ~= 100 then
-				QBCore.Functions.Notify("Turbo installation failed!", "error", 3500)
-				ClearPedTasks(playerPed)
-			else
-				QBCore.Functions.Notify("Success! Installing Turbo", "success", 3500)
-				FreezeEntityPosition(playerPed, true)
-				time = math.random(7000,10000)
-				TriggerEvent('pogressBar:drawBar', time, 'Installing Turbo')
-				Wait(time)
-				SetVehicleModKit(vehicle, 0)
-				ToggleVehicleMod(vehicle, 18, true)
-				SetVehicleEngineOn(vehicle, true, true)
-				SetVehicleDoorShut(vehicle, 4, false)
-				ClearPedTasks(playerPed)
-				FreezeEntityPosition(playerPed, false)
-				SetVehicleEngineOn(vehicle, false, false,false)
-				CurrentVehicleData = QBCore.Functions.GetVehicleProperties(vehicle)
-				TriggerServerEvent('updateVehicle', CurrentVehicleData)
-				TriggerServerEvent('doj:server:removeTurbo')
+			if Config.skillbarTurbo == "reload-skillbar" then
+				local finished = exports["reload-skillbar"]:taskBar(math.random(5000,7500),math.random(2,4))
+				if finished ~= 100 then
+					QBCore.Functions.Notify("Turbo installation failed!", "error", 3500)
+					ClearPedTasks(playerPed)
+				else
+					local finished2 = exports["reload-skillbar"]:taskBar(math.random(2500,5000),math.random(3,5))
+					if finished2 ~= 100 then
+						QBCore.Functions.Notify("Turbo installation failed!", "error", 3500)
+						ClearPedTasks(playerPed)
+					else
+						local finished3 = exports["reload-skillbar"]:taskBar(math.random(900,2000),math.random(5,7))
+						if finished3 ~= 100 then
+							QBCore.Functions.Notify("Turbo installation failed!", "error", 3500)
+							ClearPedTasks(playerPed)
+						else
+							QBCore.Functions.Notify("Success! Installing Turbo", "success", 3500)
+							FreezeEntityPosition(playerPed, true)
+							time = math.random(7000,10000)
+							TriggerEvent('pogressBar:drawBar', time, 'Installing Turbo')
+							Wait(time)
+							SetVehicleModKit(vehicle, 0)
+							ToggleVehicleMod(vehicle, 18, true)
+							SetVehicleEngineOn(vehicle, true, true)
+							SetVehicleDoorShut(vehicle, 4, false)
+							ClearPedTasks(playerPed)
+							FreezeEntityPosition(playerPed, false)
+							SetVehicleEngineOn(vehicle, false, false,false)
+							CurrentVehicleData = QBCore.Functions.GetVehicleProperties(vehicle)
+							TriggerServerEvent('updateVehicle', CurrentVehicleData)
+							TriggerServerEvent('doj:server:removeTurbo')
+						end
+					end
+				end
+			elseif Config.skillbarTurbo == "np-skillbar" then
+				local finished = exports["np-skillbar"]:taskBar(1000,math.random(3,5))
+				if finished ~= 100 then
+					QBCore.Functions.Notify("Turbo installation failed!", "error", 3500)
+					ClearPedTasks(playerPed)
+				else
+					QBCore.Functions.Notify("Success! Installing Turbo", "success", 3500)
+					FreezeEntityPosition(playerPed, true)
+					time = math.random(7000,10000)
+					TriggerEvent('pogressBar:drawBar', time, 'Installing Turbo')
+					Wait(time)
+					SetVehicleModKit(vehicle, 0)
+					ToggleVehicleMod(vehicle, 18, true)
+					SetVehicleEngineOn(vehicle, true, true)
+					SetVehicleDoorShut(vehicle, 4, false)
+					ClearPedTasks(playerPed)
+					FreezeEntityPosition(playerPed, false)
+					SetVehicleEngineOn(vehicle, false, false,false)
+					CurrentVehicleData = QBCore.Functions.GetVehicleProperties(vehicle)
+					TriggerServerEvent('updateVehicle', CurrentVehicleData)
+					TriggerServerEvent('doj:server:removeTurbo')
+				end
+			elseif Config.skillbarTurbo == "qb-skillbar" then
+				local Skillbar = exports['qb-skillbar']:GetSkillbarObject()
+				Skillbar.Start({
+					duration = math.random(2500,5000),
+					pos = math.random(10, 30),
+					width = math.random(10, 20),
+				}, function()
+					QBCore.Functions.Notify("Success! Installing Turbo", "success", 3500)
+					FreezeEntityPosition(playerPed, true)
+					time = math.random(7000,10000)
+					TriggerEvent('pogressBar:drawBar', time, 'Installing Turbo')
+					Wait(time)
+					SetVehicleModKit(vehicle, 0)
+					ToggleVehicleMod(vehicle, 18, true)
+					SetVehicleEngineOn(vehicle, true, true)
+					SetVehicleDoorShut(vehicle, 4, false)
+					ClearPedTasks(playerPed)
+					FreezeEntityPosition(playerPed, false)
+					SetVehicleEngineOn(vehicle, false, false,false)
+					CurrentVehicleData = QBCore.Functions.GetVehicleProperties(vehicle)
+					TriggerServerEvent('updateVehicle', CurrentVehicleData)
+					TriggerServerEvent('doj:server:removeTurbo')
+				end, function()
+					QBCore.Functions.Notify("Turbo installation failed!", "error", 3500)
+					ClearPedTasks(playerPed)
+				end)
 			end
 		end
 	else
