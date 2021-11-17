68283faeb2768c756624bd3e6cf598c254f8a0be
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-17 10:50:45 -0800

    added skillbar configs

--- config.lua
+++ config.lua
@@ -1,4 +1,6 @@
 Config = {}
 
 
-Config.checkTunesMessage = "chat" -- "chat" or "phone"
+Config.checkTunesMessage    = "phone"           -- Must choose ["chat" or "phone"] to recieve the tuning message
+Config.skillbarTurbo        = "qb-skillbar"     -- Must choose one of the avalible skillbars ["reload-skillbar","np-skillbar","qb-skillbar"] links are in read me
+Config.skillbarXenons       = "reload-skillbar" -- Must choose one of the avalible skillbars ["reload-skillbar","np-skillbar","qb-skillbar"] links are in read me
