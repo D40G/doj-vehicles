2c6bc08740c93ae8a35ef6c1af7951e02e2357fd
/dojwun <79622405+dojwun@users.noreply.github.com>
2021-11-18 16:44:45 -0800

    added owned vehicle check

--- config.lua
+++ config.lua
@@ -1,6 +1,14 @@
 Config = {}
 
+-- Tunes Message                                -- Must choose ["chat" or "phone"] to recieve the tuning message
+Config.checkTunesMessage    = "phone"
 
-Config.checkTunesMessage    = "phone"           -- Must choose ["chat" or "phone"] to recieve the tuning message
-Config.skillbarTurbo        = "qb-skillbar"     -- Must choose one of the avalible skillbars ["reload-skillbar","np-skillbar","qb-skillbar"] links are in read me
-Config.skillbarXenons       = "reload-skillbar" -- Must choose one of the avalible skillbars ["reload-skillbar","np-skillbar","qb-skillbar"] links are in read me
+
+--Skillbars                                     -- Must choose one of the avalible skillbars ["reload-skillbar","np-skillbar","qb-skillbar"] links are in read me
+Config.skillbarTurbo        = "qb-skillbar" 
+Config.skillbarXenons       = "qb-skillbar" 
+Config.skillbarRemoveTint   = "qb-skillbar"
+
+
+-- Main 
+Config.isVehicleOwned = true                    --  [true or false] checks if the vehicle is owned
